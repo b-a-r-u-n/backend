a3e15ecb1ab918bd5d9ae97e047fe991b9dde389
b-a-r-u-n <barunkumarmahakud40@gmail.com>
2024-09-24 15:30:05 +0530

    update utils

--- src/utils/ApiResponce.js
+++ src/utils/ApiResponce.js
@@ -4,8 +4,8 @@ class ApiREsponce {
         data,
         message = "Success"
     ){
-        this.statusCode = statusCode;
-        this.data = data;
+        this.statusCode = statusCode,
+        this.data = data,
         this.message = message,
         this.success = statusCode < 400
     }
